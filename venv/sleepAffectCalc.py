a0316264d2700bfd4c58105ff9ffb63daec2a758
gavrielsontag <36906788+gavrielsontag@users.noreply.github.com>
2018-10-25 19:46:34 -0400

    Update sleepAffectCalc.py

--- venv/sleepAffectCalc.py
+++ venv/sleepAffectCalc.py
@@ -1,6 +1,11 @@
-import unittest
+import sleepAffectCalcUnderthehood
 def main():
- print("annoying")
+ #where the data will be entered
+ iSleepAm=int(input("ideal sleep amount \n"))
+ aSleepAm = int(input("avg sleep amount \n"))
+ lSleepAm = int(input("amount slept previous night\n"))
+ #method call
+ sleepAffectCalcUnderthehood.processAlg.processAlgIdealDeviation(iSleepAm,aSleepAm,lSleepAm)
  
 
 if __name__ == '__main__':
